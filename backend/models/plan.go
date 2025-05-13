7544f2167bd2100fdf6705426deabbcb2440691e
KomSE <w513795@outlook.com>
2025-05-14 01:08:54 +0800

    增加新的表：套餐plan，用户体检信息表userplandoucment，评价表commentary

--- backend/models/plan.go
+++ backend/models/plan.go
@@ -13,7 +13,7 @@ type Plan struct {
 }
 
 // 体检套餐指标表，存储具体套餐的指标信息，item_id，item_name(主键),只保存信息，便于添加新的指标内容
-type HeathItem struct {
+type HealthItem struct {
 	gorm.Model
 	ItemName string `gorm:"type:varchar(100);not null;index;column:item_name"`
 }
@@ -21,10 +21,10 @@ type HeathItem struct {
 // plan-item对应表, 展示套餐信息
 type PlanHeathItem struct {
 	gorm.Model
-	RelationPlanId      uint `gorm:"not null;index;column:plan_id"`
-	RelationHeathItemId uint `gorm:"not null;index;column:heath_item_id"`
+	RelationPlanId       uint `gorm:"not null;index;column:plan_id"`
+	RelationHealthItemId uint `gorm:"not null;index;column:health_item_id"`
 
 	// Relations
-	ThisPlan      Plan      `gorm:"foreignKey:RelationPlanId;references:ID;constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
-	ThisHeathItem HeathItem `gorm:"foreignKey:RelationHeathItemId;references:ID;constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
+	ThisPlan      Plan       `gorm:"foreignKey:RelationPlanId;references:ID;constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
+	ThisHeathItem HealthItem `gorm:"foreignKey:RelationHealthItemId;references:ID;constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
 }
