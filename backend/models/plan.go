fb9b473cad6509a429820ceb0da4b2280cabc8fd
KomSE <w513795@outlook.com>
2025-05-14 00:16:21 +0800

    增加新的表:餐plan，用户体检信息表userplandoucment，评价表commentary, 删除Institution表中的ExaminationPackage属性 关于体检套餐，我们使用单独的套餐表plan来存储数据。

--- backend/models/plan.go
+++ backend/models/plan.go
@@ -5,11 +5,11 @@ import "gorm.io/gorm"
 // 套餐表，只存储套餐信息，plan_id，plan_name,organization_id(外键)
 type Plan struct {
 	gorm.Model
-	PlanName                string `gorm:"type:varchar(100);not null;unique;index;column:plan_name"`
-	RelattionOrganizationID uint   `gorm:"not null;index;column:organization_id"`
+	PlanName              string `gorm:"type:varchar(100);not null;unique;index;column:plan_name"`
+	RelationInstitutionID uint   `gorm:"not null;index;column:institution_id"`
 
 	// Relations
-	ThisOrganization Organization `gorm:"foreignKey:RelattionOrganizationID;references:ID;constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
+	ThisInstitution Institution `gorm:"foreignKey:RelationInstitutionID;references:ID;constraint:OnUpdate:RESTRICT,OnDelete:RESTRICT;"`
 }
 
 // 体检套餐指标表，存储具体套餐的指标信息，item_id，item_name(主键),只保存信息，便于添加新的指标内容
