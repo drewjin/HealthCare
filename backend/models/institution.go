fce1165763d807301ce6a4a214fda348dd9782a2
KomSE <w513795@outlook.com>
2025-05-15 00:30:57 +0800

    pr前的保存

--- backend/models/institution.go
+++ backend/models/institution.go
@@ -9,7 +9,7 @@ type Institution struct {
 	InstitutionName          string `json:"institution_name" gorm:"type:varchar(50);not null;unique;column:institution_name"`
 	InstitutionAddress       string `json:"institution_address" gorm:"type:varchar(100);not null;column:institution_address"`
 	InstitutionPhone         string `json:"institution_phone" gorm:"type:varchar(11);not null;column:institution_phone"`
-	InstitutionQualification string `json:"institution_qualification" gorm:"type:varchar(50);column:institution_qualification"`
+	InstitutionQualification string `json:"institution_qualification" gorm:"type:varchar(50);not null;column:institution_qualification"`
 	UserID                   uint   `json:"user_id" gorm:"not null;index;column:user_id"`
 	Status                   uint8  `json:"status" gorm:"type:tinyint(1);not null;default:0;column:status"` // 0: pending, 1: approved, 2: rejected
 
